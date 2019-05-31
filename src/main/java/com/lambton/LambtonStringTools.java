38f19cd5fbff9eff5a01a7fcc0e32db2e5522d63
ashishpratheesh <50932031+ashishpratheesh@users.noreply.github.com>
2019-05-31 18:56:28 -0400

    Initial commit

---
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,24 @@
+package com.lambton;
+
+public class LambtonStringTools
+{
+
+
+
+    public void reverse()
+    { String s1= "Lambton";
+       reverse();
+        System.out.println(s1);
+    }
+public static void binaryToDecimal()
+{
+    String s2="A";
+   int deci=Integer.parseInt(s2);
+   System.out.println(deci);
+}}
+}
+public static void initials()
+        {}
+
+}
+
---
+++ src/main/java/com/lambton/Main.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+public class Main
+{
+    public static void main(String[] args)
+
+    {
+        LambtonStringTools l = new LambtonStringTools();
+        l.reverse();
+    }
+}
+
+}
